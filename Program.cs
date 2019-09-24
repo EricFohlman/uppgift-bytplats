eac254989bd9d3f336010fd7b8a74441031c3576
EricFohlman <54439056+EricFohlman@users.noreply.github.com>
2019-09-24 13:51:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_byta_plats__power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            BytPlats(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+        }
+
+        public static void BytPlats(ref int a, ref int b)
+        {
+            int tal1 = a;
+            int tal2 = b;
+            a = tal2;
+            b = tal1;
+        }
+    }
+}
